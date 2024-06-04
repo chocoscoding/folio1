6693b62990c716fca1288e58bb1041946aa19c55
chocoscoding <chocoscoding@gmail.com>
2024-06-04 16:19:12 +0100

    changed sanity env config and 'useCdn'

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,22 +1,15 @@
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-04-27'
+export const apiVersion = process.env.SANITY_STUDIO_API_VERSION || "2024-06-04";
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+export const dataset = assertValue(process.env.SANITY_STUDIO_DATASET, "Missing environment variable: SANITY_STUDIO_DATASET");
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+export const projectId = assertValue(process.env.SANITY_STUDIO_PROJECT_ID, "Missing environment variable: SANITY_STUDIO_PROJECT_ID");
 
-export const useCdn = false
+export const useCdn = true;
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
-    throw new Error(errorMessage)
+    throw new Error(errorMessage);
   }
 
-  return v
+  return v;
 }
