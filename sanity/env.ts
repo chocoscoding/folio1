f7d0f9cd952bc2c51c266d7fa03236f4c0073445
chocoscoding <chocoscoding@gmail.com>
2024-07-11 21:53:15 +0100

    .

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,8 +1,11 @@
 export const apiVersion = process.env.SANITY_STUDIO_API_VERSION || "2024-06-04";
 
-export const dataset = assertValue(process.env.SANITY_STUDIO_DATASET, "Missing environment variable: SANITY_STUDIO_DATASET");
+export const dataset = assertValue(process.env.NEXT_PUBLIC_SANITY_DATASET, "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET");
 
-export const projectId = assertValue(process.env.SANITY_STUDIO_PROJECT_ID, "Missing environment variable: SANITY_STUDIO_PROJECT_ID");
+export const projectId = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID"
+);
 
 export const useCdn = true;
 
